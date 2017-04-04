a7c2be67671b28c2bab5752c0acaf9933c6396f5
鲜槟丞 <pzhxbz@QQ.com>
2017-04-04 17:43:28 +0800

    添加项目文件。

---
+++ PictureSortGUI/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PictureSortGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 850);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox
+            // 
+            this.listBox.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.listBox.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.listBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 78;
+            this.listBox.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.listBox.Location = new System.Drawing.Point(856, 0);
+            this.listBox.Name = "listBox";
+            this.listBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox.Size = new System.Drawing.Size(363, 780);
+            this.listBox.TabIndex = 1;
+            this.listBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox_KeyDown);
+            this.listBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.listBox_KeyPress);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(1427, 709);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(154, 71);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.Text = "下一张";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1582, 853);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "PictureSort";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button nextButton;
+    }
+}
+
---
+++ PictureSortGUI/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureSortGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            List<int> flag = new List<int>();
+            for (int i = 0; i < index.Length; i++)
+            {
+                if (isSeclet[i])
+                {
+                    flag.Add(i);
+                    isSeclet[i] = false;
+                }
+            }
+            SecletList(Keys.Enter);
+            WriteFlag(flag);
+            nowNum++;
+            if (nowNum >= fileInfo.Length)
+            {
+                MessageBox.Show("没了", "", MessageBoxButtons.OK, MessageBoxIcon.None,
+                    MessageBoxDefaultButton.Button1);
+                this.Close();
+                return;
+            }
+            var temp = fileInfo[nowNum].ToString();
+            nowImage = Image.FromFile("pictures/" + temp);
+            Bitmap map = (Bitmap)nowImage;
+            nowImage = (Bitmap)map.GetThumbnailImage(pictureBox1.Height, pictureBox1.Width, null, IntPtr.Zero);
+            if (sender == null)
+            {
+                OnPaint(null);
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo theFolder = new DirectoryInfo("pictures");
+            if (theFolder == null)
+            {
+                MessageBox.Show("没有找到pictuers文件夹", "", MessageBoxButtons.OK, MessageBoxIcon.Warning,
+                    MessageBoxDefaultButton.Button1);
+                this.Close();
+                return;
+            }
+            fileInfo = theFolder.GetFiles();
+            isSeclet = new bool[index.Length];
+            try
+            {
+                var file = new StreamReader("info.txt", Encoding.Default);
+                nowNum = int.Parse(file.ReadLine().Split(' ')[0]);
+            }
+            catch (IOException)
+            {
+                nowNum = 0;
+            }
+            var temp = fileInfo[nowNum].ToString();
+            nowImage = Image.FromFile("pictures/" + temp);
+            Bitmap map = (Bitmap)nowImage;
+            nowImage = (Bitmap)map.GetThumbnailImage(pictureBox1.Height, pictureBox1.Width, null, IntPtr.Zero);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            pictureBox1.Image = nowImage;
+        }
+
+        private void SecletList(Keys key)
+        {
+            for (int i = 0; i < index.Length; i++)
+            {
+                if (key == keyIndex[i])
+                {
+                    if (isSeclet[i] == false)
+                    {
+                        isSeclet[i] = true;
+                    }
+                    else
+                    {
+                        isSeclet[i] = false;
+                    }
+                }
+            }
+            listBox.Items.Clear();
+            for (int i = 0; i < index.Length; i++)
+            {
+                if (isSeclet[i])
+                {
+                    listBox.Items.Add(("*选中*") + this.index[i]);
+                }
+                else
+                {
+                    listBox.Items.Add(index[i]);
+                }
+            }
+        }
+
+
+
+        private FileInfo[] fileInfo;
+        private bool[] isSeclet;
+        private String[] index =
+        {
+            "0","1","2","3","4","5","6","7","8","9"
+        };
+
+        private Keys[] keyIndex =
+        {
+            Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9
+        };
+        private Char[] keyCharIndex =
+        {
+            '0','1','2','3','4','5','6','7','8','9'
+        };
+
+        private int nowNum;
+        private Image nowImage;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                nextButton_Click(null, null);
+            }
+        }
+
+        private void listBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            var key = e.KeyCode;
+            if (e.KeyCode == Keys.Enter)
+            {
+                nextButton_Click(null, null);
+                return;
+            }
+            SecletList(key);
+        }
+
+        private void listBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var a = e.KeyChar;
+        }
+
+        private void WriteFlag(List<int> flag)
+        {
+            var fs = File.OpenWrite("flag.txt");
+            fs.Position = fs.Length;
+            String line = "";
+            foreach (var x in flag)
+            {
+                line += Convert.ToString(x) + " ";
+            }
+            line += '\n';
+            Encoding encoder = Encoding.ASCII;
+            byte[] bytes = encoder.GetBytes(line);
+            fs.Write(bytes, 0, bytes.Length);
+            fs.Close();
+            FileStream file;
+            try
+            {
+                file = File.OpenWrite("states.txt");
+                byte[] Ibytes = encoder.GetBytes(Convert.ToString(nowNum) + '\n');
+                file.Write(Ibytes, 0, Ibytes.Length);
+                file.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+        }
+    }
+}
